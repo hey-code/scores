e1a37d27d5f3b0ca9886c2569dccff8776b0e4f7
Joanna <finewitch@gmail.com>
2017-06-12 19:37:01 +0200

    passed list data as props

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import './App.css';
 import List from "./List";
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      teamsList : ['amazing', 'awesome', 'terrific', 'splendid']
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -11,7 +17,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Scores. Amazing App. You'll Be Surprised. I Promise. This Copy Is Amazing. Really.</h2>
         </div>
-        <List />
+        <List data = {this.state.teamsList}/>
       </div>
     );
   }
--- src/List.js
+++ src/List.js
@@ -3,14 +3,13 @@ import React, { Component } from 'react';
 class List extends Component {
   render() {
     return (
-        <ul className="list-teams">
-          <li>amazing</li>
-          <li>awesome</li>
-          <li>terrific</li>
-          <li>splendid</li>
-        </ul>
-        )
-    }
+      <ul className="list-teams">
+        {this.props.data.map(team =>
+          <li>{team}</li>
+        )}
+      </ul>
+    )
+  }
 }
 
 export default List;
